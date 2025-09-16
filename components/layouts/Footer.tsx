3084e3b5f0a1f5363098ec7e939244feea7e3911
kumaa-n <kuma2557.guba1@gmail.com>
2025-09-16 21:22:15 +0900

    feat:フッター作成 #5

---
+++ components/layouts/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer footer-center bg-neutral text-base-100 p-5">
+      &copy; {new Date().getFullYear()} 秋フレーズジェネレーター
+    </footer>
+  )
+}
