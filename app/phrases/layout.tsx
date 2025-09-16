b005640c0cccc6f29e1dd195511be9a0745ba7c6
kumaa-n <kuma2557.guba1@gmail.com>
2025-09-17 00:20:51 +0900

    feat:文言の調整 #15

--- app/phrases/layout.tsx
+++ app/phrases/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "秋フレーズ一覧 | 秋フレーズジェネレーター",
+  title: "秋っぽいフレーズ一覧 | 秋フレーズジェネレーター",
   description: "秋っぽいフレーズをご覧いただけます",
 }
 
